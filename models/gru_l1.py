7808404f4dd1d7a1a81b8231c33de7076c746fcb
akitenkrad <akitenkrad@gmail.com>
2022-03-06 14:48:00 +0900

    rename dnn -> gru

--- models/gru_l1.py
+++ models/gru_l1.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 from utils.utils import Config
 from models.base import BaseModel
 
-class DnnL1(BaseModel):
-    '''DNN with 1 layer
+class GRU_L1(BaseModel):
+    '''GRU with 1 layer
     
     Input:
         (batch_size, sentence_length, embedding_dim)
@@ -14,18 +14,18 @@ class DnnL1(BaseModel):
     Output:
         (batch_size, n_class)
     '''
-    def __init__(self, config:Config, embedding_dim:int, n_class:int, name:str='dnn-l1'):
+    def __init__(self, config:Config, embedding_dim:int, hidden_dim:int, n_class:int, name:str='dnn-l1'):
         super().__init__(config, name)
         self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
         self.n_class = n_class 
         self.build()
 
     def build(self):
-        self.linear_1 = nn.Linear(self.embedding_dim, self.embedding_dim // 2)
-        self.layer_norm_1 = nn.LayerNorm(self.embedding_dim // 2)
-        self.linear_2 = nn.Linear(self.embedding_dim // 2, self.embedding_dim // 4)
-        self.layer_norm_2 = nn.LayerNorm(self.embedding_dim // 4)
-        self.output = nn.Linear(self.embedding_dim // 4, self.n_class)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_dim, 1)
+        self.linear_1 = nn.Linear(self.hidden_dim, self.hidden_dim // 2)
+        self.batch_norm_1 = nn.BatchNorm1d(self.hidden_dim // 2)
+        self.output = nn.Linear(self.hidden_dim // 2, self.n_class)
         self.dropout = nn.Dropout(0.2)
 
     def step(self, x: torch.Tensor, y: torch.Tensor, loss_func: Callable) -> Tuple[float, torch.Tensor]:
@@ -42,11 +42,10 @@ class DnnL1(BaseModel):
         return out
 
     def forward(self, x):
-        out = self.linear_1(x)
-        out = self.layer_norm_1(out)
-        out = self.dropout(out)
-        out = self.linear_2(out)
-        out = self.layer_norm_2(out)
+        _, hidden = self.gru(x)
+        hidden = hidden.reshape(-1, self.hidden_dim)
+        out = self.linear_1(hidden)
+        out = self.batch_norm_1(out)
         out = self.dropout(out)
         out = self.output(out)
         if self.n_class < 2:
