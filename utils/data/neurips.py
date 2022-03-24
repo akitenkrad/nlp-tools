2a65bb921ab4add1437728ab8162cd234bd3938f
akitenkrad <akitenkrad@gmail.com>
2022-03-24 15:16:50 +0900

    update download_neurips_2021

--- utils/data/neurips.py
+++ utils/data/neurips.py
@@ -1,12 +1,23 @@
+from typing import List
 from pathlib import Path
 import json
 from utils.google_drive import GDriveObjects, download_from_google_drive
 from stats.stats import Text
 
-def download_neurips_2021() -> dict:
+def download_neurips_2021() -> List[Text]:
     cache_path = Path('__cache__/datasets/neurips_2021.json')
     if not cache_path.exists():
         download_from_google_drive(GDriveObjects.NeurIPS_2021.value, str(cache_path))
-    data = json.load(open(cache_path))
-    data = [Text(summary=paper['abstract'], **paper) for paper in data]
+    json_data = json.load(open(cache_path))
+    data = []
+    for paper in json_data:
+        text = Text(title=paper['title'], summary=paper['abstract'], **{
+            'url': paper['url'],
+            'authors': paper['authors'],
+            'pdf_url': paper['pdf_url'],
+            'keywords': paper['keywords'],
+            'grade': paper['grade'],
+            'date': paper['date']
+        })
+        data.append(text)
     return data
