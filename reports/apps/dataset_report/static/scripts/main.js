22ed9c937b068cba5fb1d646e824d8b8e036f843
akitenkrad <akitenkrad@gmail.com>
2022-04-17 23:33:35 +0900

    update reports

---
+++ reports/apps/dataset_report/static/scripts/main.js
@@ -0,0 +1,1 @@
+// Topic Model - Topic Probability Distribution
--- reports/apps/dataset_report/views.py
+++ reports/apps/dataset_report/views.py
@@ -22,11 +22,32 @@ def index():
 def report(report_name):
     meta_json = json.load(open(Path(__file__).parent / 'static/reports' / report_name / 'meta.json'))
     args = {
-        'intertopic_distance_map': f'reports/{report_name}/topic_model/intertopic_distance_map/report.html',
-        'hierarchical_clustering': f'reports/{report_name}/topic_model/hierarchical_clustering/report.html',
-        'topic_word_score': f'reports/{report_name}/topic_model/barchart/report.html',
-        'similarity_matrix': f'reports/{report_name}/topic_model/similarity_matrix/report.html',
-        'topics_per_class': f'reports/{report_name}/topic_model/topics_per_class/report.html',
-        'topic_prob_dist_options': [{'text': m['text'], 'value': m['value']} for m in meta_json['topic_prob_dist']]
+        'intertopic_distance_map': {
+            'name': f'reports/{report_name}/topic_model/intertopic_distance_map/report.html',
+            'width': meta_json['topic_model']['intertopic_distance_map']['width'] + 50,
+            'height': meta_json['topic_model']['intertopic_distance_map']['height'] + 50,
+        },
+        'hierarchical_clustering': {
+            'name': f'reports/{report_name}/topic_model/hierarchical_clustering/report.html',
+            'width': meta_json['topic_model']['hierarchical_clustering']['width'] + 50,
+            'height': meta_json['topic_model']['hierarchical_clustering']['height'] + 50,
+        },
+        'topic_word_score': {
+            'name': f'reports/{report_name}/topic_model/barchart/report.html',
+            'width': meta_json['topic_model']['barchart']['width'] + 50,
+            'height': meta_json['topic_model']['barchart']['height'] + 50,
+        },
+        'similarity_matrix': {
+            'name': f'reports/{report_name}/topic_model/similarity_matrix/report.html',
+            'width': meta_json['topic_model']['similarity_matrix']['width'] + 50,
+            'height': meta_json['topic_model']['similarity_matrix']['height'] + 50,
+        },
+        'topics_per_class': {
+            'name': f'reports/{report_name}/topic_model/topics_per_class/report.html',
+            'width': meta_json['topic_model']['topics_per_class']['width'] + 50,
+            'height': meta_json['topic_model']['topics_per_class']['height'] + 50,
+        },
+        'topics': meta_json[''],
+        'topic_prob_dist': [{'title': m['title'], 'htmlfile': m['htmlfile']} for m in meta_json['topic_model']['topic_prob_dist']]
     }
-    return render_template('dataset_report/report.html', **args)
+    return render_template('dataset_report/report.j2', **args)
