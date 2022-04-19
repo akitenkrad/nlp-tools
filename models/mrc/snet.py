774560b28979b29af8b00f9dccc946b9460ff792
akitenkrad <akitenkrad@gmail.com>
2022-04-19 13:47:00 +0900

    update snet

--- models/mrc/snet.py
+++ models/mrc/snet.py
@@ -25,7 +25,7 @@ def attention_pooling(v, w1, x1, w2, x2):
     return c_q, a
 
 
-class SentenceEmbedding(nn.Module):
+class SentenceEmbeddingLayer(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -57,30 +57,25 @@ class SentenceEmbedding(nn.Module):
         return u, x_words                                                                       # (seq_len, hidden_dim, seq_len, embedding_dim)
 
 
-class EvidenceExtractor(nn.Module):
+class EvidenceExtractorLayer(nn.Module):
     def __init__(self, embedding_dim, hidden_dim):
         super().__init__()
-        self.embedding_dim = embedding_dim
-        self.hidden_dim = hidden_dim
+        self.embedding_dim = 300
+        self.hidden_dim = 150
 
-        self.v = nn.Linear(embedding_dim, 1, bias=False)
-        self.v_g = nn.Linear(embedding_dim * 2, 1, bias=False)
-        self.w_u_q = nn.Linear(embedding_dim, embedding_dim, bias=False)
-        self.w_u_p = nn.Linear(embedding_dim, embedding_dim, bias=False)
-        self.w_v_q = nn.Linear(embedding_dim, embedding_dim, bias=False)
-        self.w_v_p = nn.Linear(embedding_dim, embedding_dim, bias=False)
-        self.w_h_p = nn.Linear(embedding_dim, embedding_dim, bias=False)
-        self.w_h_a = nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.v = nn.Linear(self.embedding_dim, 1, bias=False)
+        self.v_g = nn.Linear(self.embedding_dim * 2, 1, bias=False)
+        self.w_u_q = nn.Linear(self.embedding_dim, self.embedding_dim, bias=False)
+        self.w_u_p = nn.Linear(self.embedding_dim, self.embedding_dim, bias=False)
+        self.w_v_q = nn.Linear(self.embedding_dim, self.embedding_dim, bias=False)
+        self.w_v_p = nn.Linear(self.embedding_dim, self.embedding_dim, bias=False)
+        self.w_h_p = nn.Linear(self.embedding_dim, self.embedding_dim, bias=False)
+        self.w_h_a = nn.Linear(self.embedding_dim, self.embedding_dim, bias=False)
         self.v_r_q = nn.Parameter(torch.zeros((self.embedding_dim,), dtype=torch.float32))
 
-        self.w_g = nn.Linear(embedding_dim * 2, embedding_dim * 2, bias=False)
-        self.gru1 = nn.GRU(embedding_dim * 2, hidden_dim, 1, batch_first=True, bidirectional=True)
-        self.gru2 = nn.GRU(embedding_dim, hidden_dim, 1, batch_first=True, bidirectional=True)
-
-    def init_hidden(self, hidden_dim):
-        '''initialize hidden state'''
-        # TODO add data type
-        return torch.zeros(1, 1, hidden_dim).to(device)
+        self.w_g = nn.Linear(self.embedding_dim * 2, self.embedding_dim * 2, bias=False)
+        self.gru1 = nn.GRU(self.embedding_dim * 2, self.hidden_dim, 1, batch_first=True, bidirectional=True)
+        self.gru2 = nn.GRU(self.embedding_dim, self.hidden_dim, 1, batch_first=True, bidirectional=True)
 
     def forward(self, u_q: Tensor, u_ps: List[Tensor]):
         '''
@@ -113,10 +108,7 @@ class EvidenceExtractor(nn.Module):
             v_ps.append(v_p)
 
         # Pointer Network
-        N = len(v_ps)
         # 1. predict start point
-
-        # initial hidden state "r_q"
         s = self.v(torch.tanh(self.w_u_q(u_q) + self.w_v_q(self.v_r_q)))
         a = torch.softmax(s, dim=-1)
         r_q = torch.einsum('m, mh -> h', a.squeeze(), u_q)
@@ -125,9 +117,7 @@ class EvidenceExtractor(nn.Module):
         a_1 = torch.softmax(s, dim=-1)
         p_1 = torch.argmax(a_1.squeeze())
 
-        # calculate start point
-
-        #   2. predict end point
+        # 2. predict end point
         c = torch.einsum('ij, kij -> j', a, torch.cat(v_ps, dim=0))
         _, h_t_a = self.gru2(c.reshape(1, 1, -1), r_q.reshape(2, 1, -1))
         h_t_a = h_t_a.reshape(1, 1, -1).squeeze()
@@ -136,7 +126,7 @@ class EvidenceExtractor(nn.Module):
         a_2 = F.softmax(s.squeeze(), dim=0)
         p_2 = torch.argmax(a_2)
 
-        # Passage Ranking
+        # 3. Passage Ranking
         gs = []
         for v_p in v_ps:
             s = self.v(torch.tanh(self.w_v_p(v_p) + self.w_v_q(r_q)))
@@ -144,7 +134,6 @@ class EvidenceExtractor(nn.Module):
             r_p = torch.einsum('m, mh -> h', a, v_p.squeeze())
             g = self.v_g(torch.tanh(self.w_g(torch.cat([r_q, r_p]))))
             gs.append(g)
-
         psg_ranks = torch.softmax(torch.stack(gs).squeeze(), dim=0)
 
         return (p_1, a_1), (p_2, a_2), psg_ranks
