7f7b72a5a230ef03676c2f4894be17b70ada9231
akitenkrad <akitenkrad@gmail.com>
2022-04-22 00:37:02 +0900

    update snet.py

--- datasets/base.py
+++ datasets/base.py
@@ -1,36 +1,36 @@
-from typing import Any
 from abc import ABC, abstractmethod
 from os import PathLike
 from pathlib import Path
+from typing import Any
+
 import numpy as np
-
-from torchvision.transforms import transforms
 from torch.utils.data import Dataset
-
-from utils.logger import get_logger, Logger
-from utils.utils import Phase, is_notebook, Config
+from torchvision.transforms import transforms
+from utils.logger import Logger, get_logger
+from utils.utils import Config, Phase, is_notebook
 
 if is_notebook():
     from tqdm.notebook import tqdm
 else:
     from tqdm import tqdm
 
+
 class BaseDataset(ABC, Dataset):
-    def __init__(self, config:Config, phase:Phase=Phase.TRAIN):
+    def __init__(self, config: Config, phase=Phase.TRAIN):
         super().__init__()
         self.config = config
         self.config.add_logger('dataset_log')
         self.dataset_path = Path(self.config.data.data_path)
         self.valid_size = self.config.train.valid_size
         self.test_size = self.config.train.test_size
-        self.phase = phase
+        self.phase: Phase = phase
 
-        # self.train_data, self.valid_data, self.test_data = self.__load_data__(self.dataset_path)
-        # self.dev_data = {idx: self.train_data[idx] for idx in range(1000)}
+        self.train_data, self.valid_data, self.test_data = self.__load_data__(self.dataset_path, config.train.test_size, config.train.valid_size)
+        self.dev_data = {idx: self.train_data[idx] for idx in range(1000)}
 
-    def __load_data__(self, dataset_path:PathLike, test_size:float, valid_size:float):
+    def __load_data__(self, dataset_path: PathLike, test_size: float, valid_size: float):
         '''load train and test data
-        
+
         Returns:
             train_data, valid_data, test_data: dict[index, namedtuple[data, label]]
         '''
@@ -60,15 +60,20 @@ class BaseDataset(ABC, Dataset):
     # phase change functions
     def to_train(self):
         self.phase = Phase.TRAIN
+
     def to_valid(self):
         self.phase = Phase.VALID
+
     def to_test(self):
         self.phase = Phase.TEST
+
     def to_dev(self):
         self.phase = Phase.DEV
+
     def to_submission(self):
         self.phase = Phase.SUBMISSION
-    def to(self, phase:Phase):
+
+    def to(self, phase: Phase):
         if phase == Phase.TRAIN:
             self.to_train()
         elif phase == Phase.VALID:
