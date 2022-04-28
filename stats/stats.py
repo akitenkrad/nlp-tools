4490c642a89eb6c358dc66b4ef5f081af1d64f2e
akitenkrad <akitenkrad@gmail.com>
2022-04-28 15:17:19 +0900

    update stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -37,31 +37,40 @@ class Text(object):
                 setattr(self, name, value)
 
     def __str__(self):
-        return f'<Text {self.title[:10]}...>'
+        return f"<Text {self.title[:10]}...>"
 
     def __repr__(self):
         return self.__str__()
 
-    def preprocess(self):
-        if self.lang == Lang.ENGLISH:
-            tokenizer = WordTokenizer(language=Lang.ENGLISH, remove_stopwords=True, remove_punctuations=True, stemming=True, add_tag=True)
-            text = self.title + ' ' + self.summary
+    def preprocess(self) -> str:
+        if self.language == Lang.ENGLISH:
+            tokenizer = WordTokenizer(
+                language=Lang.ENGLISH,
+                remove_stopwords=True,
+                remove_punctuations=True,
+                stemming=True,
+                add_tag=True,
+            )
+            text = self.title + " " + self.summary
             words = tokenizer.tokenize(text)
-            text = ' '.join([word[0] for word in words if word[1].startswith('N')])
+            text = " ".join([word[0] for word in words if word[1].startswith("N")])
             return text
 
-        elif self.lang == Lang.JAPANESE:
-            tagger = MeCab.Tagger(f'-d {unidic.DICDIR}')
-            result = tagger.parse(self.title + ' ' + self.summary)
+        elif self.language == Lang.JAPANESE:
+            tagger = MeCab.Tagger(f"-d {unidic.DICDIR}")
+            result = tagger.parse(self.title + " " + self.summary)
             words = []
-            for line in result.split('\n'):
-                if '\t' not in line:
+            for line in result.split("\n"):
+                if "\t" not in line:
                     continue
-                word, _attrs = line.split('\t')
-                attrs = _attrs.split(',')
-                if attrs[0] == '名詞':
+                word, _attrs = line.split("\t")
+                attrs = _attrs.split(",")
+                if attrs[0] == "名詞":
                     words.append(word)
-            return ' '.join(words)
+            return " ".join(words)
+
+        else:
+            raise RuntimeError(f"Unknown language: {self.language}")
 
 
 class DocStat(object):
@@ -82,41 +91,46 @@ class DocStat(object):
         names: Dict[int, str] = self.topic_model.topic_names
         sizes: Dict[int, int] = self.topic_model.topic_sizes
         all_topics: Dict[int, List[Tuple[str, float]]] = self.topic_model.get_topics()
-        res = {index: {'name': name, 'size': size, 'top_n_words': n_words} for (index, name), size, n_words in zip(names.items(), sizes.values(), all_topics.values())}
+        res = {
+            index: {"name": name, "size": size, "top_n_words": n_words}
+            for (index, name), size, n_words in zip(
+                names.items(), sizes.values(), all_topics.values()
+            )
+        }
         return res
 
     def analyze(self, texts: List[Text]):
-        with tqdm(total=2, desc='analyzing...', leave=False) as progress:
+        with tqdm(total=2, desc="analyzing...", leave=False) as progress:
 
             def update_progress(desc_text: str):
                 progress.update(1)
                 progress.set_description(desc_text)
 
             # Basic analysis
-            update_progress('Basic Analysis...')
+            update_progress("Basic Analysis...")
 
             # Topic Modeling
-            update_progress('Topic Modeling...')
+            update_progress("Topic Modeling...")
             texts_for_tp = [text.preprocess() for text in texts]
             topics, probs = self.topic_model.fit_transform(texts_for_tp)
-            self.topic_model_attrs['topics'] = topics
-            self.topic_model_attrs['probs'] = probs
+            self.topic_model_attrs["topics"] = topics
+            self.topic_model_attrs["probs"] = probs
 
             # Topics per Class
             keywords = [text.keywords[0] for text in texts]
             topics_per_class = self.topic_model.topics_per_class(
                 texts_for_tp, topics, classes=keywords
             )
-            self.topic_model_attrs['topics_per_class'] = topics_per_class
+            self.topic_model_attrs["topics_per_class"] = topics_per_class
 
             for topic, text, prob in zip(topics, texts, probs):
                 text.topic = topic
                 text.prob = prob
-            self.topic_model_attrs['texts'] = texts
+            self.topic_model_attrs["texts"] = texts
 
     def get_topic(self, text: Text) -> Dict[int, float]:
         topics = {idx: 0.0 for idx in self.topics.keys()}
-        _topics, _probs = self.topic_model.find_topics(text.title + '\n' + text.summary)
+        _topics, _probs = self.topic_model.find_topics(text.title + "\n" + text.summary)
         for topic, prob in zip(_topics, _probs):
             topics[topic] = prob
         return topics
