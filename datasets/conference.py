7a27ef9f8adab3aba45cd6a563b9e414e9370b1b
akitenkrad <akitenkrad@gmail.com>
2022-05-22 19:08:53 +0900

    update conference dataset

--- datasets/conference.py
+++ datasets/conference.py
@@ -2,23 +2,24 @@ import json
 import re
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from stats.stats import ConferenceText
 from utils.google_drive import GDriveObjects, download_from_google_drive
+from utils.tokenizers import Tokenizer
 from utils.utils import Lang
 
 
 class Conference(ABC):
     @classmethod
     @abstractmethod
-    def load(cls) -> List[ConferenceText]:
+    def load(cls, preprocess_tokenizer: Optional[Tokenizer]) -> List[ConferenceText]:
         pass
 
 
 class NeurIPS_2021(Conference):
     @classmethod
-    def load(self) -> List[ConferenceText]:
+    def load(cls, preprocess_tokenizer: Optional[Tokenizer]) -> List[ConferenceText]:
         data_path = Path("data/conference/NeurIPS/neurips_2021.json")
 
         if not data_path.exists():
@@ -28,10 +29,20 @@ class NeurIPS_2021(Conference):
         papers = json.load(open(data_path))
         texts = []
         for paper in papers:
+
+            title = paper["title"]
+            summary = paper["abstract"]
+
+            if preprocess_tokenizer:
+                title_tokens = preprocess_tokenizer.tokenize(title)
+                summary_tokens = preprocess_tokenizer.tokenize(summary)
+                title = " ".join(token.surface for token in title_tokens)
+                summary = " ".join(token.surface for token in summary_tokens)
+
             texts.append(
                 ConferenceText(
-                    title=paper["title"],
-                    summary=paper["abstract"],
+                    title=title,
+                    summary=summary,
                     keywords=[keyword.strip().lower() for keyword in paper["keywords"]],
                     pdf_url=paper["pdf_url"],
                     authors=paper["authors"],
@@ -43,7 +54,7 @@ class NeurIPS_2021(Conference):
 
 class ANLP_2022(Conference):
     @classmethod
-    def load(self) -> List[ConferenceText]:
+    def load(self, preprocess: Optional[Tokenizer]) -> List[ConferenceText]:
         data_path = Path("data/conference/ANLP/ANLP-2022.json")
 
         if not data_path.exists():
@@ -55,10 +66,20 @@ class ANLP_2022(Conference):
         papers = data["papers"]
         texts = []
         for paper in papers:
+
+            title = paper["title"]
+            summary = paper["abstract"]
+
+            if preprocess_tokenizer:
+                title_tokens = preprocess_tokenizer.tokenize(title)
+                summary_tokens = preprocess_tokenizer.tokenize(summary)
+                title = " ".join(token.surface for token in title_tokens)
+                summary = " ".join(token.surface for token in summary_tokens)
+
             texts.append(
                 ConferenceText(
-                    title=paper["title"],
-                    summary=paper["abstract"],
+                    title=title,
+                    summary=summary,
                     keywords=[keywords[paper["id"].split("-")[0]].strip().lower()],
                     pdf_url="",
                     authors=[],
@@ -70,7 +91,7 @@ class ANLP_2022(Conference):
 
 class JSAI_2022(Conference):
     @classmethod
-    def load(self) -> List[ConferenceText]:
+    def load(self, preprocess_tokenizer: Optional[Tokenizer]) -> List[ConferenceText]:
         data_path = Path("data/conference/JSAI/JSAI_2022.json")
 
         if not data_path.exists():
@@ -80,10 +101,19 @@ class JSAI_2022(Conference):
         papers = json.load(open(data_path))
         texts = []
         for paper in papers:
+            title = re.sub(r"^\[.+\]\s*", "", paper["title"])
+            summary = paper["summary"]
+
+            if preprocess_tokenizer:
+                title_tokens = preprocess_tokenizer.tokenize(title)
+                summary_tokens = preprocess_tokenizer.tokenize(summary)
+                title = " ".join(token.surface for token in title_tokens)
+                summary = " ".join(token.surface for token in summary_tokens)
+
             texts.append(
                 ConferenceText(
-                    title=re.sub(r"^\[.+\]\s*", "", paper["title"]),
-                    summary=paper["summary"],
+                    title=title,
+                    summary=summary,
                     keywords=[keyword.replace("Keywords:", "").strip().lower() for keyword in paper["keywords"]],
                     pdf_url="",
                     authors=paper["authors"],
