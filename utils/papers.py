21b20d7a046b349ed4bdf9e8ac8fa5225f6d3725
akitenkrad <akitenkrad@gmail.com>
2022-07-22 10:21:44 +0900

    fix a bug in papers.py

--- utils/papers.py
+++ utils/papers.py
@@ -180,8 +180,8 @@ class Papers(object):
 
     def load_index(self):
         with h5py.File(self.hdf5_path, mode="a") as hdf5:
-            indices = list(np.array(hdf5["papers/index"], dtype=Papers.HDF5_STR)) if "papers/index" in hdf5 else []
-            errors = list(np.array(hdf5["papers/errors"], dtype=Papers.HDF5_STR)) if "papers/errors" in hdf5 else []
+            indices = [item.decode("utf-8") for item in np.array(hdf5["papers/index"], dtype=Papers.HDF5_STR)] if "papers/index" in hdf5 else []
+            errors = [item.decode("utf-8") for item in np.array(hdf5["papers/errors"], dtype=Papers.HDF5_STR)] if "papers/errors" in hdf5 else []
             return indices, errors
 
     def update_index(self, indices: List[str], errors: List[str]):
