a3da71b9fe71c219791a2981c524a75f05e71e12
akitenkrad <akitenkrad@gmail.com>
2022-08-24 10:39:17 +0900

    update msmarco dataset

--- datasets/msmarco.py
+++ datasets/msmarco.py
@@ -11,6 +11,7 @@ import numpy as np
 import torch
 from embeddings.base import Embedding
 from sklearn.model_selection import train_test_split
+from utils.data import Text
 from utils.google_drive import GDriveObjects, download_from_google_drive
 from utils.utils import Config, Phase, download, is_notebook
 
@@ -21,23 +22,34 @@ if is_notebook():
 else:
     from tqdm import tqdm
 
-Passage = namedtuple('Passage', ('is_selected', 'passage_text'))
-RougeL = namedtuple('RougeL', ('start_pos', 'end_pos', 'passage_span', 'answer', 'rouge_l'))
+Passage = namedtuple("Passage", ("is_selected", "passage_text"))
+RougeL = namedtuple("RougeL", ("start_pos", "end_pos", "passage_span", "answer", "rouge_l"))
 
-MsmarcoItemX = namedtuple('MsmarcoItemX', ('query_id', 'query_type', 'query_word_tokens', 'query_char_tokens',
-                                           'passage_word_tokens', 'passage_char_tokens', 'passage_is_selected', 'answer_word_tokens', 'answer_char_tokens'))
-MsmarcoItemPos = namedtuple('MsmarcoItemPos', ('start_pos', 'end_pos'))
+MsmarcoItemX = namedtuple(
+    "MsmarcoItemX",
+    (
+        "query_id",
+        "query_type",
+        "query_word_tokens",
+        "query_char_tokens",
+        "passage_word_tokens",
+        "passage_char_tokens",
+        "passage_is_selected",
+        "answer_word_tokens",
+        "answer_char_tokens",
+    ),
+)
+MsmarcoItemPos = namedtuple("MsmarcoItemPos", ("start_pos", "end_pos"))
 
 
 class MsmarcoDatasetType(Enum):
-    TRAIN = 'train'
-    DEV = 'dev'
+    TRAIN = "train"
+    DEV = "dev"
 
 
 class MsmarcoRecord(object):
-
     def __init__(self, idx: int, item: dict, rouge_l: dict):
-        '''
+        """
         Args:
             idx: int
             item:
@@ -57,17 +69,23 @@ class MsmarcoRecord(object):
                     'answer': ANSWER,
                     'rouge_l': ROUGE-L
                 }
-        '''
+        """
         self.index = idx
-        self.query_id: int = int(item['query_id'])
-        self.query_type: str = item['query_type']
-        self.query: str = item['query']
-        self.passages: List[Passage] = [Passage(p['is_selected'], p['passage_text']) for p in item['passages']]
-        self.answers: List[str] = [answer for answer in item['answers']]
-        self.rouge_l = RougeL(int(rouge_l['start_pos']), int(rouge_l['end_pos']), str(rouge_l['passage_span']), str(rouge_l['answer']), float(rouge_l['rouge_l']))
+        self.query_id: int = int(item["query_id"])
+        self.query_type: Text = Text(item["query_type"])
+        self.query: Text = Text(item["query"])
+        self.passages: List[Passage] = [Passage(p["is_selected"], p["passage_text"]) for p in item["passages"]]
+        self.answers: List[Text] = [Text(answer) for answer in item["answers"]]
+        self.rouge_l = RougeL(
+            int(rouge_l["start_pos"]),
+            int(rouge_l["end_pos"]),
+            str(rouge_l["passage_span"]),
+            str(rouge_l["answer"]),
+            float(rouge_l["rouge_l"]),
+        )
 
     def __str__(self):
-        return f'<Record query_id:{self.query_id} query:{self.query[:10]}...>'
+        return f"<Record query_id:{self.query_id} query:{self.query[:10]}...>"
 
     def __repr__(self):
         return self.__str__()
@@ -113,17 +131,20 @@ class MsmarcoRecord(object):
         qwt, qct = self.query2tokens(embedding)
         pwt, pct, psl = self.passages2tokens(embedding)
         awt, act = self.answers2tokens(embedding)
-        x = MsmarcoItemX(torch.LongTensor(self.query_id),                                                       # query_id
-                         self.query_type,                                                                       # query_type
-                         torch.tensor(qwt, dtype=torch.float32),                                                # query_word_tokens
-                         [torch.tensor(chars, dtype=torch.float32) for chars in qct],                           # query_char_tokens
-                         [torch.tensor(passage, dtype=torch.float32) for passage in pwt],                       # passage_word_tokens
-                         [[torch.tensor(chars, dtype=torch.float32) for chars in passage] for passage in pct],  # passage_char_tokens
-                         torch.tensor(psl, dtype=torch.float32),                                                # passage_is_selected
-                         [torch.tensor(answer, dtype=torch.float32) for answer in awt],                         # answer_word_tokens
-                         [[torch.tensor(chars, dtype=torch.float32) for chars in answer] for answer in act])    # answer_char_tokens
-        y = MsmarcoItemPos(torch.LongTensor([self.rouge_l.start_pos])[0],                                       # start_pos
-                           torch.LongTensor([self.rouge_l.end_pos])[0])                                         # end_pos
+        x = MsmarcoItemX(
+            torch.LongTensor(self.query_id),  # query_id
+            self.query_type,  # query_type
+            torch.tensor(qwt, dtype=torch.float32),  # query_word_tokens
+            [torch.tensor(chars, dtype=torch.float32) for chars in qct],  # query_char_tokens
+            [torch.tensor(passage, dtype=torch.float32) for passage in pwt],  # passage_word_tokens
+            [[torch.tensor(chars, dtype=torch.float32) for chars in passage] for passage in pct],  # passage_char_tokens
+            torch.tensor(psl, dtype=torch.float32),  # passage_is_selected
+            [torch.tensor(answer, dtype=torch.float32) for answer in awt],  # answer_word_tokens
+            [[torch.tensor(chars, dtype=torch.float32) for chars in answer] for answer in act],
+        )  # answer_char_tokens
+        y = MsmarcoItemPos(
+            torch.LongTensor([self.rouge_l.start_pos])[0], torch.LongTensor([self.rouge_l.end_pos])[0]  # start_pos
+        )  # end_pos
         return x, y
 
 
@@ -142,7 +163,7 @@ class MsmarcoDataset(BaseDataset):
         # self.dev_data = {idx: self.train_data[idx] for idx in range(1000)}
 
     def __load_data__(self, dataset_path: Path) -> Tuple[MsmarcoDs, MsmarcoDs, MsmarcoDs]:
-        '''load imdb corpus dataset
+        """load imdb corpus dataset
         download tar.gz file if dataset does not exist
 
         Args:
@@ -150,53 +171,69 @@ class MsmarcoDataset(BaseDataset):
 
         Returns:
             train_data, valid_data, test_data: dict[idx, ImdbItem]
-        '''
+        """
 
         # download dataset if it does not exist
-        (dataset_path / 'msmarco').mkdir(parents=True, exist_ok=True)
-        if not (dataset_path / 'msmarco' / 'train_v2.1.json').exists():
-            self.config.log.dataset_log.info('download MSMARCO train_v2.1.json from Google Drive')
-            download_from_google_drive(GDriveObjects.MSMARCO_TRAIN_DATA.value, str(dataset_path / 'msmarco' / 'train_v2.1.json'))
-        if not (dataset_path / 'msmarco' / 'train_v2.1.rouge.json').exists():
-            self.config.log.dataset_log.info('download MSMARCO train_v2.1.rouge.json from Google Drive')
-            download_from_google_drive(GDriveObjects.MSMARCO_TRAIN_ROUGE.value, str(dataset_path / 'msmarco' / 'train_v2.1.rouge.json'))
-        if not (dataset_path / 'msmarco' / 'dev_v2.1.json').exists():
-            self.config.log.dataset_log.info('download MSMARCO dev_v2.1.json from Google Drive')
-            download_from_google_drive(GDriveObjects.MSMARCO_DEV_DATA.value, str(dataset_path / 'msmarco' / 'dev_v2.1.json'))
-        if not (dataset_path / 'msmarco' / 'dev_v2.1.rouge.json').exists():
-            self.config.log.dataset_log.info('download MSMARCO dev_v2.1.rouge.json from Google Drive')
-            download_from_google_drive(GDriveObjects.MSMARCO_DEV_ROUGE.value, str(dataset_path / 'msmarco' / 'dev_v2.1.rouge.json'))
+        (dataset_path / "msmarco").mkdir(parents=True, exist_ok=True)
+        if not (dataset_path / "msmarco" / "train_v2.1.json").exists():
+            self.config.log.dataset_log.info("download MSMARCO train_v2.1.json from Google Drive")
+            download_from_google_drive(
+                GDriveObjects.MSMARCO_TRAIN_DATA.value, str(dataset_path / "msmarco" / "train_v2.1.json")
+            )
+        if not (dataset_path / "msmarco" / "train_v2.1.rouge.json").exists():
+            self.config.log.dataset_log.info("download MSMARCO train_v2.1.rouge.json from Google Drive")
+            download_from_google_drive(
+                GDriveObjects.MSMARCO_TRAIN_ROUGE.value, str(dataset_path / "msmarco" / "train_v2.1.rouge.json")
+            )
+        if not (dataset_path / "msmarco" / "dev_v2.1.json").exists():
+            self.config.log.dataset_log.info("download MSMARCO dev_v2.1.json from Google Drive")
+            download_from_google_drive(
+                GDriveObjects.MSMARCO_DEV_DATA.value, str(dataset_path / "msmarco" / "dev_v2.1.json")
+            )
+        if not (dataset_path / "msmarco" / "dev_v2.1.rouge.json").exists():
+            self.config.log.dataset_log.info("download MSMARCO dev_v2.1.rouge.json from Google Drive")
+            download_from_google_drive(
+                GDriveObjects.MSMARCO_DEV_ROUGE.value, str(dataset_path / "msmarco" / "dev_v2.1.rouge.json")
+            )
 
         if self.ds_type == MsmarcoDatasetType.TRAIN:
-            json_data = json.load(open(dataset_path / 'msmarco' / 'train_v2.1.json'))
-            rouge_data = {int(key): value for key, value in json.load(open(dataset_path / 'msmarco' / 'train_v2.1.rouge.json')).items()}
+            json_data = json.load(open(dataset_path / "msmarco" / "train_v2.1.json"))
+            rouge_data = {
+                int(key): value
+                for key, value in json.load(open(dataset_path / "msmarco" / "train_v2.1.rouge.json")).items()
+            }
         elif self.ds_type == MsmarcoDatasetType.DEV:
-            json_data = json.load(open(dataset_path / 'msmarco' / 'dev_v2.1.json'))
-            rouge_data = {int(key): value for key, value in json.load(open(dataset_path / 'msmarco' / 'dev_v2.1.rouge.json')).items()}
+            json_data = json.load(open(dataset_path / "msmarco" / "dev_v2.1.json"))
+            rouge_data = {
+                int(key): value
+                for key, value in json.load(open(dataset_path / "msmarco" / "dev_v2.1.rouge.json")).items()
+            }
 
-        keys = list(json_data['query_id'].keys())
+        keys = list(json_data["query_id"].keys())
 
         # load rouge_l value
-        target_qids = set([r['query_id'] for r in rouge_data.values() if r['rouge_l'] >= self.rouge_threshold])
-        self.config.log.dataset_log.info(f'filter with rouge_l={self.rouge_threshold} -> total: {len(rouge_data)} target: {len(target_qids)}')
+        target_qids = set([r["query_id"] for r in rouge_data.values() if r["rouge_l"] >= self.rouge_threshold])
+        self.config.log.dataset_log.info(
+            f"filter with rouge_l={self.rouge_threshold} -> total: {len(rouge_data)} target: {len(target_qids)}"
+        )
 
         # load msmarco
         data_all: list[MsmarcoRecord] = []
-        for idx, key in tqdm(enumerate(keys), desc='loading data...', total=len(keys)):
+        for idx, key in tqdm(enumerate(keys), desc="loading data...", total=len(keys)):
 
             loaded_data = {
-                'answers': json_data['answers'][key],
-                'passages': json_data['passages'][key],
-                'query': json_data['query'][key],
-                'query_id': json_data['query_id'][key],
-                'query_type': json_data['query_type'][key],
-                'wellFormedAnswers': json_data['wellFormedAnswers'][key]
+                "answers": json_data["answers"][key],
+                "passages": json_data["passages"][key],
+                "query": json_data["query"][key],
+                "query_id": json_data["query_id"][key],
+                "query_type": json_data["query_type"][key],
+                "wellFormedAnswers": json_data["wellFormedAnswers"][key],
             }
 
-            rouge_l = rouge_data[int(json_data['query_id'][key])]
+            rouge_l = rouge_data[int(json_data["query_id"][key])]
             record = MsmarcoRecord(idx, loaded_data, rouge_l)
 
-            if int(json_data['query_id'][key]) in target_qids:
+            if int(json_data["query_id"][key]) in target_qids:
                 data_all.append(record)
 
         # split data
@@ -211,7 +248,7 @@ class MsmarcoDataset(BaseDataset):
         return train_dict, valid_dict, test_dict
 
     def __load_text__(self, path: PathLike) -> str:
-        text = open(path, 'rt').read().strip()
+        text = open(path, "rt").read().strip()
         return text
 
     def __getitem__(self, index):
@@ -222,11 +259,12 @@ class MsmarcoDataset(BaseDataset):
         elif self.phase == Phase.TEST or self.phase == Phase.SUBMISSION:
             record = self.test_data[index]
         else:
-            raise RuntimeError('Unknown Phase')
+            raise RuntimeError("Unknown Phase")
 
         return record.to_data(self.embedding)
 
     def get_record(self, index) -> MsmarcoRecord:
+        record: MsmarcoRecord
         if self.phase == Phase.TRAIN or self.phase == Phase.DEV:
             record = self.train_data[index]
         elif self.phase == Phase.VALID:
@@ -234,19 +272,16 @@ class MsmarcoDataset(BaseDataset):
         elif self.phase == Phase.TEST or self.phase == Phase.SUBMISSION:
             record = self.test_data[index]
         else:
-            raise RuntimeError('Unknown Phase')
+            raise RuntimeError("Unknown Phase")
         return record
 
     def save_reference(self, records: List[MsmarcoRecord], out_path: PathLike):
         ref_data = []
-        for record in tqdm(records, desck='Saving reference data...'):
-            _data = {
-                'query_id': record.query_id,
-                'answers': [record.answers[0]]
-            }
+        for record in tqdm(records, desck="Saving reference data..."):
+            _data = {"query_id": record.query_id, "answers": [record.answers[0]]}
             ref_data.append(json.dumps(_data, ensure_ascii=False))
-        with open(out_path, mode='w', encoding='utf-8') as wf:
-            wf.write('\n'.join(ref_data))
+        with open(out_path, mode="w", encoding="utf-8") as wf:
+            wf.write("\n".join(ref_data))
 
     @staticmethod
     def collate_fn(batch):
